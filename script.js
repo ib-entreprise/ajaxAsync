60e33b4b60e4963762bc7802ed11c2f192c85a76
ib-entreprise <bahibrahimasory590@email.com>
2023-10-24 16:01:45 +0200

    exo 1 ok

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-
-let missions = document.querySelector('#missions');
-let planets = document.querySelector('#planets');
-let vehicules = document.querySelector('#vehicules');
-let vivants = document.querySelector('#vivants');
-
-
+let filmsElement = document.querySelector('#films');
+let planetsElement = document.querySelector('#planets');
+let vehiclesElement = document.querySelector('#vehicles');
+let vivantsElement = document.querySelector('#vivants');
+ 
 onInit();
 
 async function onInit(){
     const peoples = await getData('https://swapi.dev/api/people/');
     const planets = await getData('https://swapi.dev/api/planets/');
-    console.log(planets.count);
-    displyDataOnPage(peoples.count,vivants);
-    displyDataOnPage(planets.count,planets);
+    const vehicles = await getData('https://swapi.dev/api/vehicles/');
+    const films = await getData('https://swapi.dev/api/films');
+    
+    console.log(vehicles.count);
+    console.log(films.count);
+    displyDataOnPage(peoples.count, vivantsElement);
+    displyDataOnPage(planets.count, planetsElement);    
+    displyDataOnPage(vehicles.count, vehiclesElement);    
+    displyDataOnPage(films.count, filmsElement);    
 }
-
 
 async function getData(url) {
     const response = await fetch(url);
@@ -22,7 +25,6 @@ async function getData(url) {
     return data;
 }
 
-
-function displyDataOnPage(data,item){
-    item.textContent= data;
+function displyDataOnPage(data, item){
+    item.textContent = data;
 }
