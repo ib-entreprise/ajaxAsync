8178d56a173165fe7669e60335a41a4c61c55392
ib-entreprise <bahibrahimasory590@email.com>
2023-10-26 05:58:23 +0200

    planet detail ok avec possiblilite de rechercher une planet par nom ou par terrain

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ let vehiclesElement = document.querySelector('#vehicles');
 let vivantsElement = document.querySelector('#vivants');
 let totalPlanets = document.querySelector('#totalPlanets');
 let planetsList = document.querySelector('#planetsList');
-let Oneplanet = document.querySelector('.Oneplanet');
+let Oneplanet = document.querySelectorAll('.Oneplanet');
+let search = document.querySelector('#search');
+
+
+let population = document.querySelector('#population');
+let diametre = document.querySelector('#diametre');
+let temperature = document.querySelector('#temperature');
+let terrain = document.querySelector('#terrain');
+let gravite = document.querySelector('#gravite');
+
+
+
+
 
 onInit();
 getAllPlanets();
@@ -13,9 +25,7 @@ async function onInit(){
     const planets = await getData('https://swapi.dev/api/planets/');
     const vehicles = await getData('https://swapi.dev/api/vehicles/');
     const films = await getData('https://swapi.dev/api/films');
-    
-    console.log(vehicles.count);
-    console.log(films.count);
+     
     displyDataOnPage(peoples.count, vivantsElement);
     displyDataOnPage(planets.count, planetsElement);    
     displyDataOnPage(vehicles.count, vehiclesElement);    
@@ -35,28 +45,72 @@ function displyDataOnPage(data, item){
 async function getAllPlanets(){
     const planets = await getData('https://swapi.dev/api/planets/');  
     const results = planets.results;
-    console.log(results);
+    
      for (let index = 0; index < results.length; index++) {
-        const element = results[index];   
-        console.log(element['name']);     
-        let tr = document.createElement('tr');
-        let nom = document.createElement('td');
-        let terain = document.createElement('td');
-        let url = document.createElement('td');
-
-        tr.classList.add('Oneplanet');
-        nom.textContent = element['name'];
-        terain.textContent = element['terrain'];
-        url.textContent = element['url'];
-
-        tr.appendChild(nom);
-        tr.appendChild(terain);
-        tr.appendChild(url);
-        planetsList.appendChild(tr);
+        const element = results[index];           
+        addPlanetOnTable(element)
      }
-    totalPlanets.textContent = planets.count;  
+ 
+    totalPlanets.textContent = planets.count + " resultats";  
+}
+function addPlanetOnTable(element){
+    let tr = document.createElement('tr');
+    let nom = document.createElement('td');
+    let terain = document.createElement('td');
+    
+    tr.classList.add('Oneplanet');
+    tr.id=element['url'];
+    nom.textContent = element['name'];
+    terain.textContent = element['terrain'];
+    
+
+    tr.appendChild(nom);
+    tr.appendChild(terain);
+    
+    planetsList.appendChild(tr);
 }
 
-Oneplanet.addEventListener('click', getOnePlanet);
+Oneplanet.forEach((e) => {
+    e.addEventListener('click', () => {
+        alert(e.querySelector('td').textContent);
+    });
+});
+
+
+ planetsList.addEventListener('click', async (event) => {
+    const target = event.target;
+    if (target.closest('.Oneplanet')) {
+        let tr = target.closest('.Oneplanet').id;        
+        let planet = await getData(tr);
+        displyPlanetDetail(planet);
+    }
+});
+
  
- 
+function displyPlanetDetail(element){
+    population.textContent = element['population'];
+    diametre.textContent = element['diameter'];
+    terrain.textContent = element['terrain'];
+    temperature.textContent = element['climate'];
+    gravite.textContent = element['gravity'];
+}
+
+search.addEventListener('keyup', function() {
+    const searchTerm = this.value.toLowerCase();
+    planetsList.childNodes.forEach((planet)=>{       
+        if (planet.nodeType === Node.ELEMENT_NODE) {
+            const planetName = planet.querySelector('td:first-child').textContent.toLowerCase();
+            const planetTerrain = planet.querySelector('td:last-child').textContent.toLowerCase();
+            
+             if ( planetName.includes(searchTerm) ||  planetTerrain.includes(searchTerm)) {
+                planet.style.display = 'flex';   
+                // planet.style.classList.add('right') ;
+            }else{
+                planet.style.display = 'none';                              
+             }
+        }                
+    });     
+});
+
+search.addEventListener('keyup', dispalyAllPlanetList);
+
